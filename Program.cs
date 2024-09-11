0ab04b40fa7f3a541237f04f2d179b418a3faffc
Mateus Fattori <83764798+MateusFattori@users.noreply.github.com>
2024-09-11 16:54:04 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,16 +1,31 @@
 using Microsoft.EntityFrameworkCore;
 using nextia_challenge.Data;
 using nextia_challenge.Models;
+using nextia_challenge.Repository.Interface;
+using nextia_challenge.Repository;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "nextia_challenge API", Version = "v1" });
+});
+
+
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseOracle(builder.Configuration.GetConnectionString("OracleConnection"));
 });
+
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+builder.Services.AddScoped<IPromocaoRepository, PromocaoRepository>();
 
 var app = builder.Build();
 
@@ -22,6 +37,23 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "nextia_challenge API V1");
+    });
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
